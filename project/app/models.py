964bb55af1e533300c11c435aabf941b3a056232
Rahul Lokhande <rlokhande06244@gmail.com>
2024-10-13 01:45:42 +0530

    Hotel Admin Dashboard

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Customers(models.Model):
+    cust_name=models.CharField(max_length=50)
+    cust_email=models.EmailField()
+    cust_contact=models.IntegerField()
+    cust_password=models.CharField(max_length=50)
